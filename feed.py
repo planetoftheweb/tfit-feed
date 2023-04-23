952de4d0a62e9940fc57a477d864d5d500364f5e
Ray Villalobos <ray@planetoftheweb.com>
2023-04-22 22:05:35 -0400

    update feed

--- feed.py
+++ feed.py
@@ -16,13 +16,13 @@ ET.SubElement(channel, 'description').text = data['description']
 ET.SubElement(channel, 'itunes:image', {'href': link + data['image']})
 ET.SubElement(channel, 'language').text = data['language']
 category = ET.SubElement(channel, 'itunes:category', {'text': data['category']})
-ET.SubElement(channel, 'itunes:explicit').text = data['explicit']
 ET.SubElement(channel, 'link').text = link
 ET.SubElement(channel, 'author').text = data['author']
 for item in data['item']:
     item_elem = ET.SubElement(channel, 'item')
-    ET.SubElement(item_elem, 'itunes:title').text = item['title']
-    ET.SubElement(item_elem, 'summary').text = item['summary']
+    ET.SubElement(item_elem, 'title').text = item['title']
+    ET.SubElement(item_elem, 'description').text = item['description']
+    ET.SubElement(item_elem, 'itunes:duration').text = item['duration']
     ET.SubElement(item_elem, 'pubDate').text = item['published']
     enclosure = ET.SubElement(item_elem, 'enclosure', {
         'url': link + item['file'],
