b9449c425a13072f18ed205d50c0b9a455ec319e
Ray Villalobos <ray@planetoftheweb.com>
2023-04-22 22:27:31 -0400

    changes to feed

--- feed.py
+++ feed.py
@@ -12,15 +12,17 @@ link = ET.SubElement(channel, 'link').text = data['link']
 format = ET.SubElement(channel, 'format').text = data['format']
 ET.SubElement(channel, 'title').text = data['title']
 ET.SubElement(channel, 'subtitle').text = data['subtitle']
+ET.SubElement(channel, 'itunes:author').text = data['author']
 ET.SubElement(channel, 'description').text = data['description']
 ET.SubElement(channel, 'itunes:image', {'href': link + data['image']})
 ET.SubElement(channel, 'language').text = data['language']
 category = ET.SubElement(channel, 'itunes:category', {'text': data['category']})
+ET.SubElement(channel, 'itunes:explicit').text = data['explicit']
 ET.SubElement(channel, 'link').text = link
-ET.SubElement(channel, 'author').text = data['author']
 for item in data['item']:
     item_elem = ET.SubElement(channel, 'item')
     ET.SubElement(item_elem, 'title').text = item['title']
+    ET.SubElement(item_elem, 'itunes:author').text = data['author']
     ET.SubElement(item_elem, 'description').text = item['description']
     ET.SubElement(item_elem, 'itunes:duration').text = item['duration']
     ET.SubElement(item_elem, 'pubDate').text = item['published']
